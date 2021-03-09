e4d01e6e91f5648c44500a9b1dbfdc07943028c0
emmar <emmar@WINDOWS-A74JB94.lan>
2021-03-09 09:16:34 -0500

    figured out how to loop through a folder that contains data
    Next need to figure out how I want to display all the graphs, and if I want to adjust the filtering at all because I don't  think it currently looks cleaner after vs before..

--- EEG_script_Project1.m
+++ EEG_script_Project1.m
@@ -1,11 +1,20 @@
 % EEGLab Script for CLPS0950 Group Project
 % Code written by EJR
 % comments will preceed commands 
-
+% use strcat because it removes spaces
+% use strrep to replace substrings within character array 
 EEG.etc.eeglabvers = '2021.0'; % this tracks which version of EEGLAB is being used, you may ignore it
-    data_counter = 'C:\Users\emmar\Documents\MATLAB\CLPS0950_Project1\Data\S015R07.edf' %I'll change this line when I have a loop
+pathname =  'C:\Users\emmar\Documents\MATLAB\CLPS0950_Project1\Data'; %
+
+function run_script_with_loop(pathname)
+file_struct_list = dir([pathname filesep() '*.edf']); %%get list of .edf files in the pathname specified
+
+filename_list = {file_struct_list.name}; % extract the filenames into a cellarray
+for subject = filename_list % this iterates over the elements of the cell array, one-by-one, setting the 'filename' variable like a loop variable
+    [ALLEEG EEG CURRRENTSET ALLCOM] = eeglab;
+    full_pathname = [pathname filesep() subject{1}];
     % open the data using the biosig plugin
-    EEG = pop_biosig(data_counter); 
+    EEG = pop_biosig(full_pathname); 
     % rename
     % visualize plot before doing any filtering 
      pop_eegplot( EEG, 1, 1, 1);
@@ -29,4 +38,8 @@ EEG.etc.eeglabvers = '2021.0'; % this tracks which version of EEGLAB is being us
    EEG = eeg_checkset( EEG );
    % visualize plot after filtering
      pop_eegplot( EEG, 1, 1, 1);
+     eeglab redraw %update the EEGLAB window to view changes
+end
+end
+
      
