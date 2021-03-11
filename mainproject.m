7cb7b3bb0cd927cbcaec98ab489591be4bd6da19
selinbaydar <66303385+selinbaydar@users.noreply.github.com>
2021-03-10 19:17:35 -0500

    Create mainproject.m
    This is the final version of the script that is used to call the unique color script.

---
+++ mainproject.m
@@ -0,0 +1,3 @@
+EEG.etc.eeglabvers = '2021.0'; % this tracks which version of EEGLAB is being used, you may ignore it
+pathname = 'C:\Users\emmar\Documents\MATLAB\CLPS0950_Project1\Data\trydata'; %
+Novel_script_Project1(pathname)
